c959ed66d5e5f1db04279c7c915906c637d7df0c
ingibjorghil <ingibjorg1981@gmail.com>
2017-03-30 10:46:40 +0200

    opgavedetaljer
    færdig

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,8 +1,10 @@
-$.index.open();
+
 
 function changeWindowProfil(){
 	var profil = Alloy.createController("profil").getView().open();
 };
-function changeWindowOpgaveDetaljer(){
-	var opgave = Alloy.createController("opgavedetaljer").getView().open();
-}
+function changeWindowopgave(){
+	var opgavedetaljer = Alloy.createController("opgavedetaljer").getView();
+};
+
+$.index.open();
