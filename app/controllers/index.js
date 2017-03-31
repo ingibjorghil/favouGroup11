eb6c8ad6397679159b041bd2d70645997ad14ad7
kallevonbach <kallevonbach@gmail.com>
2017-03-31 14:18:25 +0200

    Login with functionality

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,1 +1,19 @@
-$.index.open();
+var fb = require('facebook');
+
+fb.addEventListener('login', function(e) {
+    if (e.success === true) {
+        alert('login from uid: '+e.uid+', name: '+ JSON.parse(e.data).name);
+        //label.text = 'Logged In = ' + fb.loggedIn;
+    }
+    else if (e.cancelled) {
+         //user cancelled
+        alert('cancelled');
+    }
+    else {
+        alert(e.error);
+    }
+});
+
+$.loginWin.fbProxy = fb.createActivityWorker({lifecycleContainer: $.loginWin});
+
+$.loginWin.open();
