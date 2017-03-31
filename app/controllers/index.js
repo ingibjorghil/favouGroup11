566b3480dfe4150932cd7c550a168cf495f2467b
ingibjorghil <ingibjorg1981@gmail.com>
2017-03-31 16:17:27 +0200

    api model
    …

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,4 +1,14 @@
+Alloy.Collections.instance("wpUser").fetch();
 
+var user = Alloy.Models.wpuser;
+
+function registerUser(e) {
+	var params = JSON.stringify({
+		email: $.value,
+		password: $.value,
+		roles: $.value,
+	});
+}
 
 function changeWindowProfil(){
 	var profil = Alloy.createController("profil").getView().open();
